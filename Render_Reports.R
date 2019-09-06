b3c379bee9c0457d78ed3f7284a9c2733d9d5261
Claudiu Papasteri <claudiu.papasteri@gmail.com>
2019-09-06 14:01:23 +0300

    first commit batchreport

---
+++ Render_Reports.R
@@ -0,0 +1,33 @@
+## Automatically Render Multiple Reports based on Parameters
+#########################################################################
+
+# ! Here "region" is the defined parameter in .Rmd
+
+## This renders the Report for 1 value of the Parameter (e.g. "Mountain") 
+# rmarkdown::render("Report_ScriptTemplate.Rmd", 
+#                   params = list(region = "Mountain"))        
+
+## This renders the Report for all values of the Parameter
+library(purrr)
+
+render_report <- function(regionvar){
+  template <- "Report_ScriptTemplate.Rmd"
+  outfile <- sprintf("Report_ScriptTemplate_%s.html", regionvar)
+  parameters <- list(region = regionvar)
+  rmarkdown::render(template,
+                    output_dir = "docs",
+                    output_file = outfile,
+                    params = parameters)
+  invisible(TRUE)
+}
+
+# render_report("Pacific")   # TEST IT: This renders the Report for 1 value of the Parameter (e.g. "Pacific")
+
+params_list <- list(list("East North Central",                         # here set param values
+                         "East South Central", "Middle Atlantic",
+                         "Mountain", "New England", "Pacific",
+                         "South Atlantic", "West North Central",
+                         "West South Central"))
+purrr::pmap(params_list, render_report)
+
+
